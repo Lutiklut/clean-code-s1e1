1746621fa408cba7ffa8571c723eee822d080529
Lyutciya Ziganshina <lutcia_z@mail.ru>
2023-04-20 14:40:30 +0400

    fix:removed or renamed selectors with classes according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -37,23 +37,30 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.classList.add("input-task");
+
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add("input-task");
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
+    editButton.classList.add("btn");
+    editButton.classList.add("edit");
 
     deleteButton.className="delete";
+    deleteButton.classList.add("btn");
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className = "remove-icon";
     deleteButton.appendChild(deleteButtonImg);
-
-
+  
+    
     //and appending.
     listItem.appendChild(checkBox);
     listItem.appendChild(label);
     listItem.appendChild(editInput);
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
+    listItem.classList.add("list__item");
     return listItem;
 }
 
@@ -156,8 +163,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector(".edit");
+    var deleteButton=taskListItem.querySelector(".delete");
 
 
     //Bind editTask to edit button.
